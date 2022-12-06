d07a27434344349330ff4d1b9184f318c7a85691
SCXBelka <scvifixbelka@gmail.com>
2022-12-06 23:06:13 +0900

    Obnova

---
+++ Dom_zad_01/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Vvedite a: ");
+string anum = Console.ReadLine();
+int a=Convert.ToInt32(anum);
+
+Console.WriteLine("Vvedite b: ");
+string bnum = Console.ReadLine();
+int b=Convert.ToInt32(bnum);
+
+
+if (a>b)
+{
+    Console.WriteLine("a bolshee chislo i ravno:");
+    Console.WriteLine(a);
+}
+else
+{
+    Console.WriteLine("b bolshee chislo i ravno:");
+    Console.WriteLine(b);
+}
