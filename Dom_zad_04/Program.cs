e1a1a2f0792da187c692547edf95fa8f4a88e0ee
SCXBelka <scvifixbelka@gmail.com>
2022-12-07 00:30:05 +0900

    sdelal_dz4

---
+++ Dom_zad_04/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Vvedite a: ");
+string anum = Console.ReadLine();
+int a=Convert.ToInt32(anum);
+int count =2;
+
+while (count<a+1)
+{
+    if (count%2==0)
+    {
+        Console.Write(count);
+        count++;
+    }
+    else
+    {
+        
+        Console.Write(",");
+        count++;
+    }
+    
+}
+Console.Write(".");
