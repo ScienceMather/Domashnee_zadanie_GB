8c5b94589a4dd987ccc4136a49646074851dcaa3
SCXBelka <scvifixbelka@gmail.com>
2022-12-06 23:44:58 +0900

    Sdelal_dz2

---
+++ Dom_zad_02/Program.cs
@@ -0,0 +1,37 @@
+﻿int a=new Random().Next(0,99);
+int b=new Random().Next(0,99);
+int c=new Random().Next(0,99);
+int max= 0;
+if (a>b)
+{
+    if (a>c)
+    {
+        Console.WriteLine("a samoe bolshoe!");
+        max=a;
+
+    }
+    else
+    {
+        Console.WriteLine("c samoe bolshoe!");
+        max=c;
+    }
+}
+
+else
+
+    if (b>c)
+    {
+        Console.WriteLine("b samoe bolshoe!");
+        max=b;
+
+    }
+    else
+    {
+        Console.WriteLine("c samoe bolshoe!");
+        max=c;
+    }
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+Console.WriteLine("Max ravno: ");
+Console.WriteLine(max);
