f6279a38da60bc9467c56166334ab961bc56ef22
SCXBelka <scvifixbelka@gmail.com>
2022-12-07 00:06:47 +0900

    do_tretee_dz3

---
+++ Dom_zad_03/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Vvedite a: ");
+string anum = Console.ReadLine();
+double a=Convert.ToDouble(anum);
+if (a%2==0) 
+{
+    Console.WriteLine("Chetnoe!");
+    Console.WriteLine(a);
+}
+else
+{
+    Console.WriteLine("Nechetnoe!");
+    Console.WriteLine(a);
+}
