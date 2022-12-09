8624c69fe1d6b709aca0fd1d7e5c46cf822c3ab5
SCXBelka <scvifixbelka@gmail.com>
2022-12-09 23:46:05 +0900

    zadacha_pro_vtoryu_cifry

---
+++ Dom_zad_05/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Vvedite trehznachnoe chislo: ");
+int triple = int.Parse(Console.ReadLine());
+double result_10 = triple%10;
+double result_100 =triple-result_10;
+double result_2 = result_100/10;
+double result_1 = result_2%10;
+Console.WriteLine($"Vtoraya cifra {result_1}!");
