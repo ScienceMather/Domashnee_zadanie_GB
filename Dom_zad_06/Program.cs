c50c34743d7126e3781ff2cac581a772b45eca62
SCXBelka <scvifixbelka@gmail.com>
2022-12-10 00:53:23 +0900

    plohaya rabota

---
+++ Dom_zad_06/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Vvedite trehznachnoe chislo: ");
+int chislo = int.Parse(Console.ReadLine());
+int count =1;
+while (count<2)
+{
+    double result_1 = chislo%10;
+    double result_2 =chislo-result_1;
+    double result_3 = result_2/10;
+    Console.WriteLine($"Vtoraya cifra {result_3}!");
+    count++;
+}
