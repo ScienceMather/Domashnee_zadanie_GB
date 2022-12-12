fe4f241ad135c245fc2d8743afb1419e3d74d885
SCXBelka <scvifixbelka@gmail.com>
2022-12-13 00:02:22 +0900

    ispravil domashky

--- Dom_zad_05/Program.cs
+++ Dom_zad_05/Program.cs
@@ -1,7 +1,5 @@
 ﻿Console.Write("Vvedite trehznachnoe chislo: ");
 int triple = int.Parse(Console.ReadLine());
-double result_10 = triple%10;
-double result_100 =triple-result_10;
-double result_2 = result_100/10;
-double result_1 = result_2%10;
-Console.WriteLine($"Vtoraya cifra {result_1}!");
+int res1 = triple/10;
+int res2 = res1%10;
+Console.WriteLine($"Vtoraya cifra {res2}!");
