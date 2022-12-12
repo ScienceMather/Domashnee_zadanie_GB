8fef969fe9ad50228f8a42509d5c9f13b210ae27
SCXBelka <scvifixbelka@gmail.com>
2022-12-13 00:17:48 +0900

    napisal programy pro opredeleniya dnya nedeli

---
+++ Dom_zad_07/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Vvedite chislo nedeli: ");
+int chislo = int.Parse(Console.ReadLine());
+if (chislo==6 || chislo==7) Console.Write("Da eto vihodnoi");
+else Console.Write("Net eto ne vihodnoi");
