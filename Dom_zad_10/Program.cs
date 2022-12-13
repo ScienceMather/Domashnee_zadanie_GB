96ccd046ab2faa0e31b2b0dd2d59ca744f58f392
SCXBelka <scvifixbelka@gmail.com>
2022-12-13 23:51:18 +0900

    zadacha pro kybi chisel ot nulya do N

---
+++ Dom_zad_10/Program.cs
@@ -0,0 +1,22 @@
+﻿
+int EnterData(string text_cord)
+{
+    Console.WriteLine(text_cord);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int N = EnterData("Vvedite chislo N: ");
+int count =1; 
+
+for(int i =1;i<=N;i++)
+    if (count!=N)
+        {
+            Console.Write(Math.Pow(count,3)+",");
+            count++;
+        }
+        else
+        {
+            Console.Write(Math.Pow(count,3)+".");
+            count++;
+        }
