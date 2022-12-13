e0590c1197b201da4c70d7140ef4b8259e63fa20
SCXBelka <scvifixbelka@gmail.com>
2022-12-13 23:47:31 +0900

    zadacha pro opredelenie palidroma

---
+++ Dom_zad_08/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Vvedite pyatiznachnoe chislo: ");
+string chislo = Console.ReadLine();
+
+int srez = chislo.Length;
+bool T = false;
+
+for (int i =0;i<srez/2;i++)
+{
+    T = false;
+    if (chislo[i]==chislo[srez-i-1])  T = true;
+}
+
+if (T) Console.WriteLine($"{chislo} Eto palindrom!");
+else  Console.WriteLine($"{chislo} Eto ne palindrom!");
