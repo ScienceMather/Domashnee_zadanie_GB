317365c46f7931d8ee86b88cc0539141ee39d32c
SCXBelka <scvifixbelka@gmail.com>
2022-12-17 00:06:40 +0900

    summa cifr chisla

---
+++ Dom_zad_12/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+int SummDigit(int a)
+{
+    int count =0;
+    int schet =0;
+    while (true)
+    {
+        if (a!=0)
+        {
+            schet= a%10;
+            count=count+schet;
+            a= a/10;
+        }
+        else break;
+    }
+    return count;
+
+}
+
+
+int GetNumber(string text)
+{
+    Console.WriteLine(text);
+    int numb =int.Parse(Console.ReadLine());
+    return numb;
+}
+
+
+int chislo = GetNumber("Vvedite chislo: ");
+int sum_chisl = SummDigit(chislo);
+Console.WriteLine($"summa chisel = {sum_chisl}");
