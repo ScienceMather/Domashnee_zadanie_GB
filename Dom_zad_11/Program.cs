06a724737d2bc76b2add4aaa0c465abebd1db160
SCXBelka <scvifixbelka@gmail.com>
2022-12-16 23:31:48 +0900

    napisal metod dlya  nahojdeniya stepeni chisel

---
+++ Dom_zad_11/Program.cs
@@ -0,0 +1,24 @@
+﻿
+int SummNumb(int a,int b)
+{
+    int pro =1;
+    for (int i = 1; i <=b; i++) pro= pro*a;
+    return pro;
+}
+
+
+int GetNumbers(string text)
+{
+    Console.WriteLine(text);
+    int numb =int.Parse(Console.ReadLine());
+    return numb;
+}
+
+
+
+int numA = GetNumbers("Vvedite chislo A: ");
+int numB = GetNumbers("Vvedite chislo B: ");
+int power = SummNumb(numA,numB);
+Console.WriteLine($"Chislo {numA} v stepeni {numB} ravno {power}!");
+
+
