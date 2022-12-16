e75bc46e80307449a8ea842800e4045edd0c66e7
SCXBelka <scvifixbelka@gmail.com>
2022-12-17 00:09:19 +0900

    ozdaet massiv i zapol-et ego chislami ot 99 do 99

---
+++ Dom_zad_13/Program.cs
@@ -0,0 +1,18 @@
+﻿
+int GetNumber(string text)
+{
+    Console.WriteLine(text);
+    int numb =int.Parse(Console.ReadLine());
+    return numb;
+}
+
+
+int chislo = GetNumber("Vvedite dliny massiva: ");
+
+int [] mass = new int [chislo];
+for (int i = 0; i < mass.Length; i++)
+{
+    mass[i] = new Random().Next(-101,100);
+    // Console.Write($"{mass[i]} ");
+}
+Console.WriteLine($"[{string.Join(",",mass)}]");
