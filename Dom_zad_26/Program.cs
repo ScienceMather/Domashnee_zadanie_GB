d9b052dc004a65937a66d7b56bbfe0e55a0998c0
SCXBelka <scvifixbelka@gmail.com>
2023-01-02 00:46:47 +0900

    zadacha vivod ot n do 1

---
+++ Dom_zad_26/Program.cs
@@ -0,0 +1,24 @@
+﻿
+int GetNumber(string text)
+{
+    Console.WriteLine(text);
+    int numb =int.Parse(Console.ReadLine());
+    return numb;
+}
+
+int Rekyrsion(int rek)
+{
+
+    if (rek==1) return rek;
+    else
+    {
+        Console.Write($",{rek-1}");
+        Rekyrsion(rek-1);
+        return rek;
+    }
+    
+}
+
+int rekyrs =GetNumber("VVedite N: ");
+int result =Rekyrsion(chislo,stepen);
+// Console.WriteLine($"{result}");
