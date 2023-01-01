43bfbdac0a7529ce19a4060e9ec13af7eee16057
SCXBelka <scvifixbelka@gmail.com>
2023-01-02 00:48:01 +0900

    zadacha pro symmy intervala ot m do n rekyrsiya

---
+++ Dom_zad_27/Program.cs
@@ -0,0 +1,26 @@
+﻿int GetNumber(string text)
+{
+    Console.WriteLine(text);
+    int numb =int.Parse(Console.ReadLine());
+    return numb;
+}
+
+
+int Rekyrsion(int start,int end)
+{
+    if (start==end) return start;
+    else
+    {
+        end=end%10+Rekyrsion(start,end-1);
+        // Console.Write($"{end},");
+        return end;
+    }
+    
+}
+
+
+
+int start =GetNumber("VVedite SN: ");
+int end =GetNumber("VVedite EN: ");
+int result =Rekyrsion(start,end);
+Console.WriteLine($"{result}");
